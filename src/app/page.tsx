351ed68210e365f8f7db57ec63f7fd94aadebe6a
mohsindev369 <mohsinakrambukhari@gmail.com>
2024-08-17 01:27:16 +0500

    changed twitter handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect(`https://x.com/MohsinDev369_`);
+  redirect(`https://x.com/TheMohsinAkram`);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Redirecting to x...</div>
