fc1717091e0528c5fbfff0048b376aed3fda4ce1
mohsindev369 <mohsinakrambukhari@gmail.com>
2024-09-26 18:00:17 +0500

    readt to review2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import { redirect } from "next/navigation";
+'use client'
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  redirect(`https://x.com/intent/user?screen_name=TheMohsinAkram`);
+  const router = useRouter();
+  router.push("https://x.com/intent/user?screen_name=TheMohsinAkram");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Redirecting to x...</div>
